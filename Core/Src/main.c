77979c0f4cb101a3d42587837f4678b86896575e
klim405 <klim405.develop@gmail.com>
2024-12-17 15:27:25 +0300

    Добавлена основаня логика приложения

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "ring_buffer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -39,13 +39,39 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+#define TX6_BUFFER_SIZE 100
 
+#define I2C_TIMEOUT 200
+#define KEY_BOARD_ADDRESS 0xE2
+#define KB_GPIO_IN_REG 0x00
+#define KB_GPIO_OUT_REG 0x01
+#define KB_GPIO_INV_REG 0x02
+#define KB_CONFIG_REG 0x03
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+char __uart6_tx_buff[TX6_BUFFER_SIZE];
+RingBuffer uart6_tx_buff;
+bool UART6_TX_IsReady = true;
+char uart6_tx_byte_buff[1];
 
+uint8_t i2c_byte_buff[1];
+uint8_t keybord_lines_buff[4];
+int8_t keyboard_curr_line = -1;
+
+char KeyBoard_PressedSymbol;
+bool KeyBoard_HasPressedSymbol = false;
+
+typedef struct __UserInput {
+  int16_t operand[2];
+  uint16_t curr_op;
+  int8_t dec_place;
+  char action;
+} UserInputTypeDef;
+
+UserInputTypeDef UserInput;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -56,6 +82,196 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+/* UART6 ---------------------------------------------------------------------*/
+/**
+ * @brief Пытается отправить данные из кольцевого буфера
+ */
+void UART6_TryToTransmit_IT() {
+  if (UART6_TX_IsReady && !RingBuffer_IsEmpty(&uart6_tx_buff)) {
+    RingBuffer_Read(&uart6_tx_buff, uart6_tx_byte_buff, 1);
+    UART6_TX_IsReady = false;
+    HAL_UART_Transmit_IT(&huart6, (uint8_t*) uart6_tx_byte_buff, 1);
+  } 
+}
+
+/**
+ * @brief Отправляет байт в режиме прерываний
+ * @return true - байт записан в буфер отправки
+ * @return false - буфер отправки переполнен
+ */
+bool UART6_TransmitByte(char byte) {
+  bool result = false;
+  if (!uart6_tx_buff.isFull) {
+      RingBuffer_Write(&uart6_tx_buff, &byte, 1);
+      result = true;
+    }
+  return result;
+}
+
+/**
+ * @brief Отправляет строку по uart
+ * @return true - данные записан в буфер отправки
+ * @return false - буфер отправки переполнен
+ */
+bool UART6_TransmitString(char* str) {
+  bool result = false;
+  if (!uart6_tx_buff.isFull) {
+    RingBuffer_Write(&uart6_tx_buff, str, strlen(str));
+  }
+  return result;
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef* UartHandle) {
+  if (UartHandle == &huart6) {
+    UART6_TX_IsReady = true;
+  }
+}
+
+void SetError(void) {
+  UART6_TransmitString("\nerror\n");
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+}
+
+void ResetError(void) {
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+/* Key Board & I2C -----------------------------------------------------------*/
+char KeyBoard_DecodeKey(int8_t key_number) {
+  if (key_number >= 0 && key_number < 9) {
+    return ((char) key_number) + '1';
+  }
+  switch (key_number) {
+    case 10: return '0';
+    case 11: return '=';
+    case 12: return '+';
+    case 13: return '-';
+    case 14: return '*';
+    case 15: return '/';
+    default: return '\0';
+  }
+}
+
+void KeyBoard_DefineSymbol() {
+  int8_t curr_key = 0;
+  uint8_t curr_line;
+  int8_t pressed_keys[12] = { -1 };
+  int8_t total_pressed_keys = 0;
+  uint8_t masking_line = 0;
+
+  for (int8_t i = 0; i < 4; i++) {
+    curr_line = keybord_lines_buff[i];
+    for (int8_t j = 0; j < 3; j++) {
+      masking_line = curr_line && (1 << (j + 4));
+      if (masking_line == 0) {
+        pressed_keys[total_pressed_keys] = i * 4 + j;
+        ++total_pressed_keys;
+      }
+    }
+  }
+
+  if (total_pressed_keys > 2) return;
+  if (total_pressed_keys == 2 && pressed_keys[0] != 9 && pressed_keys[1] != 9) return;
+  if (total_pressed_keys == 1 && pressed_keys[0] == 9) return;
+
+  if (total_pressed_keys == 2) {
+    curr_key = pressed_keys[0] + pressed_keys[1] - 9 + 12;
+  } else {
+    curr_key = pressed_keys[0];
+  }
+
+  char currSym = KeyBoard_DecodeKey(curr_key);
+  if (currSym != KeyBoard_PressedSymbol) {
+    KeyBoard_PressedSymbol = currSym;
+    KeyBoard_HasPressedSymbol = true;
+  } 
+}
+
+void I2C1_RunUpdating() {
+  *i2c_byte_buff = 0;
+  HAL_I2C_Mem_Write_IT(&hi2c1, KEY_BOARD_ADDRESS, KB_GPIO_OUT_REG, 1, i2c_byte_buff, 1);
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_TypeDef* hi2c) {
+  if (hi2c == &hi2c1) {
+    if (keyboard_curr_line < 0) {
+      keyboard_curr_line = 0;
+      *i2c_byte_buff = ~1;
+      HAL_I2C_Mem_Write_IT(&hi2c1, KEY_BOARD_ADDRESS, KB_CONFIG_REG, 1, i2c_byte_buff, 1);
+    } else {
+      HAL_I2C_Mem_Read_IT(&hi2c1, KEY_BOARD_ADDRESS, KB_GPIO_IN_REG, 1, i2c_byte_buff, 1);
+    }
+  }
+}
+
+void HAL_I2C_MemTxCpltCallback(I2C_TypeDef* hi2c) {
+  if (hi2c == &hi2c1) {
+    keybord_lines_buff[keyboard_curr_line] = *i2c_byte_buff;
+    if (keyboard_curr_line == 3) {
+      keyboard_curr_line = 0;
+    } else {
+      ++keyboard_curr_line;
+    }
+    *i2c_byte_buff = ~(1 << keyboard_curr_line);
+    HAL_I2C_Mem_Write_IT(&hi2c1, KEY_BOARD_ADDRESS, KB_CONFIG_REG, 1, i2c_byte_buff, 1);
+  }
+}
+
+void ResetInput(void) {
+  UserInput.operand[0] = 0;
+  UserInput.operand[1] = 0;
+  UserInput.curr_op = 0;
+  UserInput.dec_place = 0;
+}
+
+void PrintResult(void) {
+  int16_t a = UserInput.operand[0];
+  int16_t b = UserInput.operand[1];
+  int32_t result = 0;
+  char result_str[32];
+  switch (UserInput.action) {
+    case '+': 
+      result = a + b;
+      break;
+    case '-':
+      result = a - b;
+      break;
+    case '*': 
+      result = a * b;
+      break;
+    case '/':
+      result = a / b;
+      break;
+  }
+  if (result < INT16_MIN || result > INT16_MAX || (UserInput.action == '/' && b == 0)) {
+    SetError();
+    ResetInput();
+  } else {
+    UART6_TransmitString(itoa(result, result_str, 10));
+    UART6_TransmitByte('\n');
+    ResetInput();
+  }
+}
+
+void ProcessInput(char sym) {
+  if (isdigit(sym) && UserInput.dec_place++ < 5) {
+    ResetError();
+    UserInput.operand[UserInput.curr_op] = UserInput.operand[UserInput.curr_op] * 10 + (int16_t) (sym - '0');
+    if (UserInput.operand[UserInput.curr_op] < 0) {
+      SetError();
+      ResetInput();
+    }
+  } else if ((sym == '+' || sym == '-' || sym == '/' || sym == '*') && UserInput.curr_op == 0) {
+    UserInput.dec_place = 0;
+    ++UserInput.curr_op;
+    UserInput.action = sym;
+  } else if (sym == '=' && UserInput.curr_op == 1) {
+    PrintResult();
+  } else {
+    SetError();
+    ResetInput();
+  }
+}
 
 /* USER CODE END 0 */
 
@@ -96,8 +312,15 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  I2C1_RunUpdating();
   while (1)
   {
+    UART6_TryToTransmit_IT();
+    if (KeyBoard_HasPressedSymbol && KeyBoard_PressedSymbol != '\0') {
+      UART6_TransmitByte(KeyBoard_PressedSymbol);
+      KeyBoard_HasPressedSymbol = false;
+      ProcessInput(KeyBoard_PressedSymbol);
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
